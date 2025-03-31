467d00f09b4209c88852b0180c7c1a846c02477f
ashutoshpythoncs@gmail.com <ashutoshpythoncs@gmail.com>
2025-03-31 17:40:36 +0530

    aashuu

--- src/pages/data/WorkData.js
+++ src/pages/data/WorkData.js
@@ -12,27 +12,6 @@ import DevDisplayPhoto2 from "../../resources/images/work-data/DevDisplay2.png";
 import DevDisplayPhoto3 from "../../resources/images/work-data/DevDisplay3.png";
 import DevDisplayPhoto4 from "../../resources/images/work-data/DevDisplay4.png";
 
-// import project3 from "../../resources/images/work-data/project-1.png";
-// import project3Banner from "../../resources/images/work-data/project-11.png";
-// import project3Photo1 from "../../resources/images/work-data/project-12.png";
-// import project3Photo2 from "../../resources/images/work-data/project-13.png";
-// import project3Photo3 from "../../resources/images/work-data/project-14.png";
-// import project3Photo4 from "../../resources/images/work-data/project-15.png";
-
-// import project4 from "../../resources/images/work-data/project-1.png";
-// import project4Banner from "../../resources/images/work-data/project-11.png";
-// import project4Photo1 from "../../resources/images/work-data/project-12.png";
-// import project4Photo2 from "../../resources/images/work-data/project-13.png";
-// import project4Photo3 from "../../resources/images/work-data/project-14.png";
-// import project4Photo4 from "../../resources/images/work-data/project-15.png";
-
-// import project5 from "../../resources/images/work-data/project-1.png";
-// import project5Banner from "../../resources/images/work-data/project-11.png";
-// import project5Photo1 from "../../resources/images/work-data/project-12.png";
-// import project5Photo2 from "../../resources/images/work-data/project-13.png";
-// import project5Photo3 from "../../resources/images/work-data/project-14.png";
-// import project5Photo4 from "../../resources/images/work-data/project-15.png";
-
 const WorkData = [
   {
     id: "uniqual",
@@ -73,47 +52,5 @@ const WorkData = [
     },
     nextProject: null,
   },
-  // {
-  //   id: "project3",
-  //   image: project3,
-  //   bannerImage: project3Banner,
-  //   name: "Uniqual ITech",
-  //   title: "UniQual iTech | The Powerhouse That Delivers High Quality IT",
-  //   slug: "uniqual-itech-the-powerhouse-that-delivers-high-quality-it",
-  //   liveLink: "https://uniqualitech.com/",
-  //   image1: project3Photo1,
-  //   image2: project3Photo2,
-  //   image3: project3Photo3,
-  //   image4: project3Photo4,
-  //   nextProject: null,
-  // },
-  // {
-  //   id: "project4",
-  //   image: project4,
-  //   bannerImage: project4Banner,
-  //   name: "Uniqual ITech",
-  //   title: "UniQual iTech | The Powerhouse That Delivers High Quality IT",
-  //   slug: "uniqual-itech-the-powerhouse-that-delivers-high-quality-it",
-  //   liveLink: "https://uniqualitech.com/",
-  //   image1: project4Photo1,
-  //   image2: project4Photo2,
-  //   image3: project4Photo3,
-  //   image4: project4Photo4,
-  //   nextProject: null,
-  // },
-  // {
-  //   id: "project5",
-  //   image: project5,
-  //   bannerImage: project5Banner,
-  //   name: "Uniqual ITech",
-  //   title: "UniQual iTech | The Powerhouse That Delivers High Quality IT",
-  //   slug: "uniqual-itech-the-powerhouse-that-delivers-high-quality-it",
-  //   liveLink: "https://uniqualitech.com/",
-  //   image1: project5Photo1,
-  //   image2: project5Photo2,
-  //   image3: project5Photo3,
-  //   image4: project5Photo4,
-  //   nextProject: null,
-  // },
 ];
 export default WorkData;
