759876226e1ca9b822e6825e6f254f8df63cb87e
ashutoshpythoncs@gmail.com <ashutoshpythoncs@gmail.com>
2025-03-31 09:45:22 +0530

    aashuu

--- src/pages/data/WorkData.js
+++ src/pages/data/WorkData.js
@@ -5,6 +5,34 @@ import uniqualPhoto2 from "../../resources/images/work-data/project-13.png";
 import uniqualPhoto3 from "../../resources/images/work-data/project-14.png";
 import uniqualPhoto4 from "../../resources/images/work-data/project-15.png";
 
+import project2 from "../../resources/images/work-data/project-1.png";
+import project2Banner from "../../resources/images/work-data/project-11.png";
+import project2Photo1 from "../../resources/images/work-data/project-12.png";
+import project2Photo2 from "../../resources/images/work-data/project-13.png";
+import project2Photo3 from "../../resources/images/work-data/project-14.png";
+import project2Photo4 from "../../resources/images/work-data/project-15.png";
+
+import project3 from "../../resources/images/work-data/project-1.png";
+import project3Banner from "../../resources/images/work-data/project-11.png";
+import project3Photo1 from "../../resources/images/work-data/project-12.png";
+import project3Photo2 from "../../resources/images/work-data/project-13.png";
+import project3Photo3 from "../../resources/images/work-data/project-14.png";
+import project3Photo4 from "../../resources/images/work-data/project-15.png";
+
+import project4 from "../../resources/images/work-data/project-1.png";
+import project4Banner from "../../resources/images/work-data/project-11.png";
+import project4Photo1 from "../../resources/images/work-data/project-12.png";
+import project4Photo2 from "../../resources/images/work-data/project-13.png";
+import project4Photo3 from "../../resources/images/work-data/project-14.png";
+import project4Photo4 from "../../resources/images/work-data/project-15.png";
+
+import project5 from "../../resources/images/work-data/project-1.png";
+import project5Banner from "../../resources/images/work-data/project-11.png";
+import project5Photo1 from "../../resources/images/work-data/project-12.png";
+import project5Photo2 from "../../resources/images/work-data/project-13.png";
+import project5Photo3 from "../../resources/images/work-data/project-14.png";
+import project5Photo4 from "../../resources/images/work-data/project-15.png";
+
 const WorkData = [
   {
     id: "uniqual",
@@ -18,6 +46,62 @@ const WorkData = [
     image2: uniqualPhoto2,
     image3: uniqualPhoto3,
     image4: uniqualPhoto4,
+    nextProject: null,
+  },
+  {
+    id: "project2",
+    image: project2,
+    bannerImage: project2Banner,
+    name: "Uniqual ITech",
+    title: "UniQual iTech | The Powerhouse That Delivers High Quality IT",
+    slug: "uniqual-itech-the-powerhouse-that-delivers-high-quality-it",
+    liveLink: "https://uniqualitech.com/",
+    image1: project2Photo1,
+    image2: project2Photo2,
+    image3: project2Photo3,
+    image4: project2Photo4,
+    nextProject: null,
+  },
+  {
+    id: "project3",
+    image: project3,
+    bannerImage: project3Banner,
+    name: "Uniqual ITech",
+    title: "UniQual iTech | The Powerhouse That Delivers High Quality IT",
+    slug: "uniqual-itech-the-powerhouse-that-delivers-high-quality-it",
+    liveLink: "https://uniqualitech.com/",
+    image1: project3Photo1,
+    image2: project3Photo2,
+    image3: project3Photo3,
+    image4: project3Photo4,
+    nextProject: null,
+  },
+  {
+    id: "project4",
+    image: project4,
+    bannerImage: project4Banner,
+    name: "Uniqual ITech",
+    title: "UniQual iTech | The Powerhouse That Delivers High Quality IT",
+    slug: "uniqual-itech-the-powerhouse-that-delivers-high-quality-it",
+    liveLink: "https://uniqualitech.com/",
+    image1: project4Photo1,
+    image2: project4Photo2,
+    image3: project4Photo3,
+    image4: project4Photo4,
+    nextProject: null,
+  },
+  {
+    id: "project5",
+    image: project5,
+    bannerImage: project5Banner,
+    name: "Uniqual ITech",
+    title: "UniQual iTech | The Powerhouse That Delivers High Quality IT",
+    slug: "uniqual-itech-the-powerhouse-that-delivers-high-quality-it",
+    liveLink: "https://uniqualitech.com/",
+    image1: project5Photo1,
+    image2: project5Photo2,
+    image3: project5Photo3,
+    image4: project5Photo4,
     nextProject: null,
   },
 ];
