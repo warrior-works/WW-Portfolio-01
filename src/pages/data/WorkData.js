6e19280c7d24bd83e0cc3a9db6851af16ed2c60b
ashutoshpythoncs@gmail.com <ashutoshpythoncs@gmail.com>
2025-03-31 20:12:04 +0530

    aashuu

--- src/pages/data/WorkData.js
+++ src/pages/data/WorkData.js
@@ -1,16 +1,23 @@
 import uniqual from "../../resources/images/work-data/UNIQUALTHUMBNAIL.png";
 import uniqualBanner from "../../resources/images/work-data/UNIQUALBANNER.png";
-import uniqualPhoto1 from "../../resources/images/work-data/UNIQUAL1.png";
-import uniqualPhoto2 from "../../resources/images/work-data/UNIQUAL2.png";
-import uniqualPhoto3 from "../../resources/images/work-data/UNIQUAL3.png";
-import uniqualPhoto4 from "../../resources/images/work-data/UNIQUAL4.png";
+import uniqual1 from "../../resources/images/work-data/UNIQUAL1.png";
+import uniqual2 from "../../resources/images/work-data/UNIQUAL2.png";
+import uniqual3 from "../../resources/images/work-data/UNIQUAL3.png";
+import uniqual4 from "../../resources/images/work-data/UNIQUAL4.png";
 
 import DevDisplay from "../../resources/images/work-data/DevDisplayTHUMBNAIL.png";
-import DevDisplayBanner from "../../resources/images/work-data/DevDisplayBanner.png";
-import DevDisplayPhoto1 from "../../resources/images/work-data/DevDisplay1.png";
-import DevDisplayPhoto2 from "../../resources/images/work-data/DevDisplay2.png";
-import DevDisplayPhoto3 from "../../resources/images/work-data/DevDisplay3.png";
-import DevDisplayPhoto4 from "../../resources/images/work-data/DevDisplay4.png";
+import DevDisplayBanner from "../../resources/images/work-data/DevDisplayBANNER.png";
+import DevDisplay1 from "../../resources/images/work-data/DevDisplay1.png";
+import DevDisplay2 from "../../resources/images/work-data/DevDisplay2.png";
+import DevDisplay3 from "../../resources/images/work-data/DevDisplay3.png";
+import DevDisplay4 from "../../resources/images/work-data/DevDisplay4.png";
+
+import VS from "../../resources/images/work-data/VSTHUMBNAIL.png";
+import VSBanner from "../../resources/images/work-data/VSBANNER.png";
+import VS1 from "../../resources/images/work-data/VS1.png";
+import VS2 from "../../resources/images/work-data/VS2.png";
+import VS3 from "../../resources/images/work-data/VS3.png";
+import VS4 from "../../resources/images/work-data/VS4.png";
 
 const WorkData = [
   {
@@ -21,10 +28,10 @@ const WorkData = [
     title: "UniQual iTech | The Powerhouse That Delivers High Quality IT",
     slug: "uniqual-itech-the-powerhouse-that-delivers-high-quality-it",
     liveLink: "https://uniqualitech.com/",
-    image1: uniqualPhoto1,
-    image2: uniqualPhoto2,
-    image3: uniqualPhoto3,
-    image4: uniqualPhoto4,
+    image1: uniqual1,
+    image2: uniqual2,
+    image3: uniqual3,
+    image4: uniqual4,
     detailsInfo: "Crafted a dynamic company website with PHP and Laravel on the backend, while spearheading frontend development using HTML, CSS, and JavaScript. Focused on optimizing user experience by implementing responsive design, interactive features, and ensuring seamless navigation. Translated design concepts into a visually appealing and functional website, contributing to a polished and engaging digital presence.",
     techStack: {
       frontend: "HTML, CSS, JavaScript",
@@ -40,10 +47,30 @@ const WorkData = [
     title: "DevDisplay - Paradise for Developers",
     slug: "devdisplay-paradise-for-developers",
     liveLink: "https://www.devdisplay.org/",
-    image1: DevDisplayPhoto1,
-    image2: DevDisplayPhoto2,
-    image3: DevDisplayPhoto3,
-    image4: DevDisplayPhoto4,
+    image1: DevDisplay1,
+    image2: DevDisplay2,
+    image3: DevDisplay3,
+    image4: DevDisplay4,
+    detailsInfo: "Developed an innovative platform for developers to showcase their projects and collaborate. Leveraged React.js for a dynamic frontend and Node.js with Express for a robust backend. Integrated MongoDB for efficient data storage and retrieval. Focused on creating a seamless user experience with real-time updates and responsive design.",
+    techStack: {
+      frontend: "React.js",
+      backend: "Node.js, Express",
+      database: "MongoDB",
+    },
+    nextProject: null,
+  },
+  {
+    id: "Visionary Architects",
+    image: VS,
+    bannerImage: VSBanner,
+    name: "Visionary Architects",
+    title: "Visionary Architects",
+    slug: "visionary-architects",
+    liveLink: "https://visionary-architects.vercel.app/",
+    image1: VS1,
+    image2: VS2,
+    image3: VS3,
+    image4: VS4,
     detailsInfo: "Developed an innovative platform for developers to showcase their projects and collaborate. Leveraged React.js for a dynamic frontend and Node.js with Express for a robust backend. Integrated MongoDB for efficient data storage and retrieval. Focused on creating a seamless user experience with real-time updates and responsive design.",
     techStack: {
       frontend: "React.js",
