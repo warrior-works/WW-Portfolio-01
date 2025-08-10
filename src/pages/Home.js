e4ee1965a65b16f0a1cdb7f6d1a5f54c10a514bf
ashutoshpythoncs@gmail.com <ashutoshpythoncs@gmail.com>
2025-08-11 00:28:49 +0530

    aashuu

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -32,7 +32,7 @@ const Home = () => {
                   <img src={aashuu} alt="" />
                 </div>
                 <div className="infos">
-                  <h4>Founder, Engineer & Designer</h4>
+                  <h4>Founder • Engineer • Designer</h4>
                   <h1>Ashutosh Kumar</h1>
                   <p>Turning pixels into production-ready products for the past 3 years with a mind full of fucking obsessed creativity.</p>
                   {/* <p>SDE I @LPS • GSoC'25 • Building DevDisplay - A tech ecosystem running on 3 business models: Platform, Labs, and Community.</p> */}
